1740449e734d08603f11a987457a223b65742ce2
joe reynolds <arma-joe@hotmail.com>
2013-09-11 07:39:24 -0400

    update to Nex Core
    Fixes the Minute/Hr and Week ban commands. Command usage examples: !10mb
    testusr you're banned for 10 mins, !5hb testusr you're banned for 5
    hours, !1wb testusr you're banned for 1 week

--- src/jeffadkins51/Nex/Nex.java
+++ src/jeffadkins51/Nex/Nex.java
@@ -8,30 +8,54 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Nex extends JavaPlugin implements Listener {
 
 	/**
 	 * Format:
-	 * PLAYER:BANTIME
+	 * PLAYER:BANTIME:BANNEDTIME
+	 * TODO:
+	 * 
 	 */
-	ArrayList<String> banList = new ArrayList<String>();
+	public static ArrayList<String> banList = new ArrayList<String>();
+	
+	public static boolean isBanned(Player p)
+	{
+		boolean result = false;
+		for (int i = 0; i < banList.size(); i++){ if (banList.contains(p.getDisplayName())){result=true;} }
+		return result;
+	}
 	
 	public void onEnable() {
 		Bukkit.getPluginManager().registerEvents(this, this);
 	}
 	
 	@EventHandler
+	public void onJoin(PlayerJoinEvent evt){
+		//Simple ban check
+		if (isBanned(evt.getPlayer())){evt.getPlayer().kickPlayer("You're banned from the server!");}
+	}
+	
+	@EventHandler
 	public void onChat(AsyncPlayerChatEvent evt) {
 		String[] args = evt.getMessage().split(" ");
 		Player p = evt.getPlayer();
-		if (args[0].equalsIgnoreCase("!10mb") && evt.getPlayer().hasPermission("Nex.10mb")){
+		if (args[0].startsWith("!") && args[0].contains("mb") || args[0].contains("hb") || args[0].contains("wb") && evt.getPlayer().hasPermission("Nex.10mb")){
 			evt.setCancelled(true);
+			String[] lnData = null;
 			if (args.length >= 2)
 			{
 				String[] ln = args[0].split("!");
-				String[] lnData = ln[1].split("mb");
+				if (args[0].contains("mb"))
+				{
+					lnData = ln[1].split("mb");
+				} else if (args[0].contains("hb")){
+					lnData = ln[1].split("hb");
+				} else if (args[0].contains("wb")){
+					lnData = ln[1].split("wb");
+				}
 				String reason = " ";
 				for (int i = 2; i < args.length; i++) reason+=args[i] + " ";
 				banList.add(args[1] + ":" + lnData[0]);
@@ -39,7 +63,7 @@ public class Nex extends JavaPlugin implements Listener {
 				p.sendMessage(ChatColor.GREEN + "You have successfully banned " + args[1] + " for (10) Minutes");
 				Bukkit.broadcastMessage(ChatColor.RED + "Player '" + args[1] + "' has been banned for (10) Minutes by " + evt.getPlayer().getDisplayName() + ". Reason:" + reason);
 			} else {
-				evt.getPlayer().sendMessage("Invalid argument count, correct usage: !10mb username reason");
+				evt.getPlayer().sendMessage("Invalid argument count, correct usage: !<ban_time><mb> username reason");
 			}
 		}
 	}
